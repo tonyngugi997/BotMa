d824fe13f8339bb54f3703bb3787078a4f3625ba
tonyngugi997 <tonyngugi997@gmail.com>
2026-04-10 10:25:15 +0300

    feat: extract and preview email body using clean_email_body

--- cleaning.py
+++ cleaning.py
@@ -11,3 +11,26 @@ def decode_subject(subject):
         else:
             decoded_string += str(part)
     return decoded_string
+
+
+def clean_email_body(email_message):
+    """Extract plain text from email, remove HTML"""
+    if email_message.is_multipart():
+        for part in email_message.walk():
+            if part.get_content_type() == "text/plain":
+                body_bytes = part.get_payload(decode=True)
+                return body_bytes.decode('utf-8', errors='replace')
+        
+        for part in email_message.walk():
+            if part.get_content_type() == "text/html":
+                body_bytes = part.get_payload(decode=True)
+                body_text = body_bytes.decode('utf-8', errors='replace')
+                body_text = body_text.replace('<br>', '\n')
+                import re
+                body_text = re.sub(r'<[^>]+>', '', body_text)
+                return body_text
+    else:
+        body_bytes = email_message.get_payload(decode=True)
+        return body_bytes.decode('utf-8', errors='replace')
+    
+    return "No body found"
