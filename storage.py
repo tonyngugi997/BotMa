5b7f079e712394d89057183e32f6fe3a291ea44e
tonyngugi997 <tonyngugi997@gmail.com>
2026-04-10 10:05:03 +0300

    add save_email function to store processed email IDs

--- storage.py
+++ storage.py
@@ -12,3 +12,11 @@ def init_database():
     ''')
     conn.commit()
     conn.close()
+
+
+def save_email(email_id):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO processed_emails (email_id) VALUES (?)', (email_id,))
+    conn.commit()
+    conn.close()
