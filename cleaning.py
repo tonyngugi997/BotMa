df63c0aa162ef56b25f1af48199d08b844178138
tonyngugi997 <tonyngugi997@gmail.com>
2026-04-10 10:49:10 +0300

    feat: aggressive HTML cleaning - remove style, script, tables, signatures

--- cleaning.py
+++ cleaning.py
@@ -12,25 +12,67 @@ def decode_subject(subject):
             decoded_string += str(part)
     return decoded_string
 
-
 def clean_email_body(email_message):
-    """Extract plain text from email, remove HTML"""
+    """Extract plain text from email, aggressively clean HTML garbage"""
+    
+    body_text = ""
+    
     if email_message.is_multipart():
         for part in email_message.walk():
             if part.get_content_type() == "text/plain":
                 body_bytes = part.get_payload(decode=True)
-                return body_bytes.decode('utf-8', errors='replace')
-        
-        for part in email_message.walk():
-            if part.get_content_type() == "text/html":
+                body_text = body_bytes.decode('utf-8', errors='replace')
+                break
+            elif part.get_content_type() == "text/html":
                 body_bytes = part.get_payload(decode=True)
                 body_text = body_bytes.decode('utf-8', errors='replace')
-                body_text = body_text.replace('<br>', '\n')
-                import re
-                body_text = re.sub(r'<[^>]+>', '', body_text)
-                return body_text
     else:
         body_bytes = email_message.get_payload(decode=True)
-        return body_bytes.decode('utf-8', errors='replace')
+        body_text = body_bytes.decode('utf-8', errors='replace')
     
-    return "No body found"
+    if not body_text:
+        return "No body found"
+    
+
+    #html cleaning
+    import re
+    
+    body_text = re.sub(r'<style[^>]*>.*?</style>', ' ', body_text, flags=re.DOTALL)
+    
+    body_text = re.sub(r'<script[^>]*>.*?</script>', ' ', body_text, flags=re.DOTALL)
+    
+    body_text = re.sub(r'<!--.*?-->', ' ', body_text, flags=re.DOTALL)
+    
+    body_text = re.sub(r'<table[^>]*>', ' ', body_text)
+    body_text = re.sub(r'</table>', ' ', body_text)
+    body_text = re.sub(r'<tr[^>]*>', ' ', body_text)
+    body_text = re.sub(r'</tr>', ' ', body_text)
+    body_text = re.sub(r'<td[^>]*>', ' ', body_text)
+    body_text = re.sub(r'</td>', ' ', body_text)
+    
+    body_text = re.sub(r'<[^>]+>', ' ', body_text)
+    
+    import html
+    body_text = html.unescape(body_text)
+    
+    lines = body_text.split('\n')
+    cleaned_lines = []
+    for line in lines:
+        if not line.strip().startswith('>'):
+            cleaned_lines.append(line)
+    body_text = '\n'.join(cleaned_lines)
+    
+    signature_markers = ['-- ', '---', 'Sent from my', 'Best regards', 'Cheers,', 'Thanks,']
+    for marker in signature_markers:
+        if marker in body_text:
+            body_text = body_text.split(marker)[0]
+    
+    # Clean up whitespace
+    body_text = re.sub(r'\n\s*\n', '\n\n', body_text)  # Multiple newlines to double
+    body_text = re.sub(r'[ \t]+', ' ', body_text)       # Multiple spaces/tabs to single space
+    body_text = re.sub(r'\n[ \t]+', '\n', body_text)    # Spaces after newline
+    body_text = re.sub(r'^\s+', '', body_text, flags=re.MULTILINE)  # Leading spaces on lines
+    body_text = re.sub(r'\s+$', '', body_text, flags=re.MULTILINE)  # Trailing spaces on lines
+    body_text = re.sub(r'\n{3,}', '\n\n', body_text)    # More than 2 newlines down to 2
+    
+    return body_text.strip()
